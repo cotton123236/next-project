14f6994d09d9f51e9c5e8c461c62f283899bd400
wilson <wilson@begonia-design.com.tw>
2023-01-07 18:26:13 +0800

    🚀 - create next app

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+// const StylelintPlugin = require('stylelint-webpack-plugin');
+
+const nextConfig = {
+  reactStrictMode: true,
+  // webpack: (config) => {
+  //   config.plugins.push(new StylelintPlugin());
+  //   return config;
+  // }
+}
+
+module.exports = nextConfig
